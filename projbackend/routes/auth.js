9883cc2ff3dc1cce8e27734713ed0563b73dd986
azamn07 <azamali.shaikh@gmail.com>
2020-05-08 23:04:03 +0530

    user Authentication Done with validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,12 +1,16 @@
 
 var express = require("express");
 var router = express.Router();
+const {check, validationResult} = require("express-validator");
 
-const {signup} = require("../controllers/auth");
-const {signout} = require("../controllers/auth");
+const {signup,signout} = require("../controllers/auth");
 
 
-router.post("/signup", signup);
+router.post("/signup", [
+    check("name","name should be atleast 3 character").isLength({ min: 3 }),
+    check("email","email is required").isEmail(),
+    check("password","password should be atlest 3 character").isLength({ min: 3 }),
+], signup);
 router.get("/signout", signout);
 
 module.exports = router;
