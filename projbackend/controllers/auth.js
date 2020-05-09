a174215e529f72fb38734df8bba2f41d91dd7975
azamn07 <azamali.shaikh@gmail.com>
2020-05-09 10:34:57 +0530

    Authentication Done.

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,11 +1,13 @@
 
 const User = require("../models/user");
 const {check, validationResult} = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 
-
+// signup Controllers
 exports.signup = (req, res) => {
 
-    const errors = validationResult(req)
+    const errors = validationResult(req);
 
     if(!errors.isEmpty()){
         return res.status(422).json({
@@ -28,10 +30,80 @@ exports.signup = (req, res) => {
    });
 };
 
+// signin Controllers
+exports.signin = (req,res) => {
+    const errors = validationResult(req); 
 
-exports.signout = (req, res) => {
-    // res.send("user signout success");        // this a way also to send message
-    res.json({                                  // this is a json type send the message to the server in key value pair
-        message: "user Signout"   
+    const { email,password } = req.body;
+
+       
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error:errors.array()[0].msg
+        });
+    }
+
+    User.findOne({email},(err, user) => {
+        if(err || !user){
+            return res.status(400).json({
+                error: "USER email is does not exists"
+            });
+        }
+
+        if(!user.authenticate(password)){
+           return res.status(401).json({
+                error: "Email and password do not match"
+            });
+        }
+
+        // create token
+        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+        // put token in cookie
+        res.cookie("token", token, {expire:new Date() + 9999 });
+
+        //send response on front end
+        const {_id, name,email,role} = user;
+        return res.json({token, user: {_id, name, email, role} });
+
     });
+
+};
+
+
+// signout controller
+exports.signout = (req, res) => { 
+    // res.send("user signout success"); 
+    res.clearCookie("token");       // this a way also to send message
+    res.json({                                  // this is a json type send the message to the server in key value pair
+        message: "User Signout successfully."   
+    });
+};
+
+// protected routes
+
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+});
+
+
+// custom middlewares
+
+exports.isAuthenticated = (req, res, next) => {
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+    if(!checker){
+        return res.status(403).json({
+            error: "ACCESS DENIED"
+        });
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error:"You Are Not Admin, ACCESS DENIED";
+        });
+    }
+    next();
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -3,14 +3,33 @@ var express = require("express");
 var router = express.Router();
 const {check, validationResult} = require("express-validator");
 
-const {signup,signout} = require("../controllers/auth");
+const {signup,signout,signin,isSignedIn} = require("../controllers/auth");
 
-
+// signup route
 router.post("/signup", [
     check("name","name should be atleast 3 character").isLength({ min: 3 }),
     check("email","email is required").isEmail(),
     check("password","password should be atlest 3 character").isLength({ min: 3 }),
-], signup);
+
+], 
+signup);
+
+// signin route
+router.post("/signin", [
+    check("email","email is required").isEmail(),
+    check("password","password field is required.").isLength({ min: 1 }),
+
+], 
+signin);
+
+//signout route
 router.get("/signout", signout);
+
+
+// router.get("/testroute", isSignedIn, (req, res) => {
+//     res.json(req.auth);
+// });
+
+
 
 module.exports = router;
